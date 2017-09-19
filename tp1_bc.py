017f1b05917204929fcee3ae6cf5b521c5ba02ad
Pedro Cantarutti <pedro.cantarutti@sisqualis.com.br>
2017-09-19 10:47:07 -0300

    Problem 1d solved.

--- tp1_bc.py
+++ tp1_bc.py
@@ -2,11 +2,24 @@
 # 1 d)
 # Resposta: %, sim
 def p_1d():
+    perc = {
+        'A': 0,
+        'G': 0,
+        'C': 0,
+        'T': 0
+    }
+
     with open('sequence.fasta', 'r') as f:
         seq = f.read().strip()
 
-        if '\n' in seq:
-            seq = seq.replace('\n', '')
+        seq = seq.replace('\n', '')
+        seq = seq.replace('N', '')
+
+        for c in seq:
+            if c in perc:
+                perc[str(c)] += 1
+
+        print(perc)
 
 
 # 1 c)
@@ -62,5 +75,6 @@ def p_1a():
 
 
 if __name__=='__main__':
-    p_1b()
-    p_1c()
+    # p_1b()
+    # p_1c()
+    p_1d()
