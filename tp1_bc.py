b0c62b5088383c73c57f8b7905fd8389de22467f
Pedro Cantarutti <pedro.cantarutti@sisqualis.com.br>
2017-09-19 08:05:48 -0300

    Second problem, done!

--- tp1_bc.py
+++ tp1_bc.py
@@ -1,14 +1,21 @@
 
 # 2A)
+# Resposta: 1127
 with open('sequence.fasta', 'r') as f:
     seq = f.read().strip()
+
     chunks, chunk_size = len(seq), 9
     a = [seq[i:i + int(chunk_size)] for i in range(0, chunks, int(chunk_size))]
-    print('CHUNK', a)
-    # for line in lines:
-    #     if 'ACCTGGTGTTCCCA' in line:
-    #         print('LINHA:', line)
 
+    pal = []
+    for s in a:
+        if s == s[::-1]:
+            if '\n' in s:
+                s = s.replace('\n', '')
+            if s not in pal:
+                pal.append(s)
+
+    print(len(pal))
 
 
 # 1A)
