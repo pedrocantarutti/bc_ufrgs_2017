30e6db3fce4a7f0c5e8807239c3afa25ba397ca5
Pedro Cantarutti <pedro.cantarutti@sisqualis.com.br>
2017-09-19 08:47:46 -0300

    A bit better resolution.

--- tp1_bc.py
+++ tp1_bc.py
@@ -1,26 +1,56 @@
 
-# 2A)
-# Resposta: 1127
-with open('sequence.fasta', 'r') as f:
-    seq = f.read().strip()
+# 1 d)
+# Resposta: %, sim
+def p_1d():
+    with open('sequence.fasta', 'r') as f:
+        seq = f.read().strip()
 
-    chunks, chunk_size = len(seq), 9
-    a = [seq[i:i + int(chunk_size)] for i in range(0, chunks, int(chunk_size))]
-
-    pal = []
-    for s in a:
-        if s == s[::-1]:
-            if '\n' in s:
-                s = s.replace('\n', '')
-            if s not in pal:
-                pal.append(s)
-
-    print(len(pal))
+        if '\n' in seq:
+            seq = seq.replace('\n', '')
 
 
-# 1A)
-# with open('sequence.fasta', 'r') as f:
-#     lines = f.readlines()
-#     for line in lines:
-#         if 'ACCTGGTGTTCCCA' in line:
-#             print('LINHA:', line)
+# 1 c)
+def p_1c():
+    with open('sequence.fasta', 'r') as f:
+        seq = f.read().strip()
+
+        if '\n' in seq:
+            seq = seq.replace('\n', '')
+
+
+# 1 b)
+# Resposta: 986
+def p_1b():
+    with open('sequence.fasta', 'r') as f:
+        seq = f.read().strip()
+
+        if '\n' in seq:
+            seq = seq.replace('\n', '')
+
+        chunks, chunk_size = len(seq), 9
+        a = [seq[i:i + int(chunk_size)] for i in range(0, chunks, int(chunk_size))]
+
+        pal = []
+        for s in a:
+            if s == s[::-1]:
+                if s not in pal and 'N' not in s:
+                    pal.append(s)
+
+        print(len(pal))
+
+
+# 1 a)
+def p_1a():
+    with open('sequence.fasta', 'r') as f:
+        lines = f.readlines()
+
+        for line in lines:
+            if '\n' in line:
+                line = line.replace('\n', '')
+            if 'ACCTGGTGTTCCCA' in line:
+                print('LINHA:', line)
+
+
+
+if __name__=='__main__':
+    p_1b()
